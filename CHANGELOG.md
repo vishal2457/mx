e462af31127df93168b3059759dd37127dae4575
vishal acharya <vishalacharya814@gmail.com>
2024-05-05 15:30:15 +0530

    feat(release)
    ci release configuration updated
    
    321

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# 1.0.0 (2024-5-5)
+
+
+* feat(api) ([55ffe14](https:/Users/vishalacharya/Documents/node-projects/maximus//commit/55ffe14ea07b3d922944fa51b8baa29b375233de))
+
+
+### BREAKING CHANGES
+
+* space added in express
+
+123
--- release.config.js
+++ release.config.js
@@ -37,11 +37,24 @@ function getCIConfig() {
     repositoryUrl: getLocalRepoUrl(),
     branches: ['main'],
     plugins: [
-      '@semantic-release/commit-analyzer',
+      [
+        "@semantic-release/commit-analyzer",
+        {
+          "preset": "angular",
+          "releaseRules": [
+            { "type": "docs", "scope": "README", "release": "patch" },
+            { "type": "refactor", "release": "patch" },
+            { "type": "style", "release": "patch" }
+          ],
+          "parserOpts": {
+            "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+          }
+        }
+      ],
       '@semantic-release/release-notes-generator',
       '@semantic-release/changelog',
-      '@semantic-release/npm',
-      '@semantic-release/github',
+      // '@semantic-release/npm',
+      // '@semantic-release/github',
     ],
   };
 }
