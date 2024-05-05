7a73f71305cb96fdc8716bd34490ffc74d126ad6
vishal acharya <vishalacharya814@gmail.com>
2024-05-05 14:47:41 +0530

    feat(release) release config added with semantic release

--- apps/api/src/express-app.ts
+++ apps/api/src/express-app.ts
@@ -6,7 +6,6 @@ import routerv1 from './routes/v1/router';
 import { logHttpRequests } from './shared/logger/morgan-logger';
 
 const app = express();
-console.log('test');
 app
   .use('/static', express.static(path.join(__dirname, '../src/public')))
   .use(express.json())
---
+++ release.config.js
@@ -0,0 +1,53 @@
+const { execSync } = require('child_process');
+
+module.exports = isDryRun() ? getDryRunConfig() : getCIConfig();
+
+function getDryRunConfig() {
+  return {
+    repositoryUrl: getLocalRepoUrl(),
+    branches: getCurrentBranch(),
+    generateNotes : ['@semantic-release/release-notes-generator'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        "@semantic-release/changelog",
+        {
+          "changelogFile": "CHANGELOG.md"
+        }
+      ]
+    ],
+  };
+}
+
+function getCIConfig() {
+  // contains your normal semantic-release config
+  // this will be used on your CI environment
+  return {
+    repositoryUrl: getLocalRepoUrl(),
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/github',
+    ],
+  };
+}
+
+function isDryRun() {
+  return process.argv.includes('--dry-run');
+}
+
+function getLocalRepoUrl() {
+  const topLevelDir = execSync('git rev-parse --show-toplevel')
+    .toString()
+    .trim();
+
+  return `file://${topLevelDir}/.git`;
+}
+
+function getCurrentBranch() {
+  return execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+}
