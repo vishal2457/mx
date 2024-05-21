d59436849437576817fa73125e3a2c712720e323
vishal2457 <vishalacharya814@gmail.com>
2024-05-21 22:51:37 +0530

    fix(api) optional filters fixes in getall api

--- apps/api/src/db/utils-db/pg/list-filters.ts
+++ apps/api/src/db/utils-db/pg/list-filters.ts
@@ -1,13 +1,15 @@
 import { eq } from 'drizzle-orm';
 import { db } from '../../db';
+import { safeParse } from '../../../../../../libs/helpers/src';
 
 export const getListQueryWithFilters = (schema, options) => {
-  const { filters, limit, offset } = options;
+  const { filters: incomingFilters, limit, offset } = options;
+  const filters = safeParse(incomingFilters);
 
   const query = db.select().from(schema).$dynamic();
 
   // add where conditions
-  if (filters?.length) {
+  if (Array.isArray(filters) && filters?.length) {
     for (const filter of filters) {
       query.where(eq(schema[filter.field], filter.value));
     }
