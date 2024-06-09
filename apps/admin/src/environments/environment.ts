e9a6544ff5a8b99deae78cfcb6b2f3f662afd0fb
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2024-06-10 00:03:03 +0530

    fix(deploy) env changed for admin

--- apps/admin/src/environments/environment.ts
+++ apps/admin/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'host.internal.docker:3000',
+  api: 'http://194.238.17.220:3000',
   get assetsURL() {
     return `${this.api}/static`;
   },
