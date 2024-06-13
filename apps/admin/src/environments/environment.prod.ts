8be23592e22cee359abcf0990fee31a090f1c035
vishal2457 <vishalacharya814@gmail.com>
2024-06-13 17:18:47 +0530

    fix(admin) responsive issues fixed, limit option commented from grid

--- apps/admin/src/environments/environment.prod.ts
+++ apps/admin/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
+
 export const environment = {
   production: true,
   api: 'http://undefined:3000',
   get assetsURL() {
-    return 'this.api' + '/static';
+    return 'this.api'+'/static';
   },
-  latestBuildTime: 'Thu Jun 13 2024, 16:31',
 };
