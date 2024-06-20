688663593c6091a5cb0bd4c3ad8040f5f936b122
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2024-06-20 22:52:48 +0530

    chore: migrations g

---
+++ apps/api/drizzle/0005_late_vertigo.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "config" ALTER COLUMN "whatsappLink" SET DEFAULT '';--> statement-breakpoint
+ALTER TABLE "config" ALTER COLUMN "youtubeLink" SET DEFAULT '';--> statement-breakpoint
+ALTER TABLE "config" ALTER COLUMN "facebookLink" SET DEFAULT '';
