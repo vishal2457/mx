2e422b68602bd1d268d5ac0129bf0f1b69af09ac
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2024-06-20 23:14:33 +0530

    chore: migrations fixed

--- apps/api/drizzle/0004_absurd_norman_osborn.sql
+++ apps/api/drizzle/0004_absurd_norman_osborn.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "config" ADD COLUMN "whatsappLink" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "config" ADD COLUMN "youtubeLink" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "config" ADD COLUMN "facebookLink" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "match" ADD COLUMN "glImage" text NOT NULL;
+ALTER TABLE "config" ADD COLUMN "whatsappLink" text DEFAULT '' NOT NULL;--> statement-breakpoint
+ALTER TABLE "config" ADD COLUMN "youtubeLink" text DEFAULT '' NOT NULL;--> statement-breakpoint
+ALTER TABLE "config" ADD COLUMN "facebookLink" text DEFAULT '' NOT NULL;--> statement-breakpoint
+ALTER TABLE "match" ADD COLUMN "glImage" text DEFAULT '' NOT NULL;
