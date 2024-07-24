a059a2fa8055316ed00f58feb6827f93f9c77a90
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2024-07-24 23:15:31 +0530

    chore: migrations added

---
+++ apps/api/drizzle/0020_greedy_dagger.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "workoutTemplateDetail" ALTER COLUMN "restBwRepsInS" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "workoutTemplateDetail" ALTER COLUMN "timeInM" SET DATA TYPE integer;
