1a11b22dc46c37ff08f0e4250ba0dae42e45ace8
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2024-08-27 20:09:50 +0530

    chore: set env updated

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -20,7 +20,7 @@ const formatter = new Intl.DateTimeFormat([], options);
 const indianTime = formatter.format(new Date());
 
 const envs = {
-  api: `http://${process.env.NODE_HOST}:3001`,
+  api: `http://server.fitflow.in`,
   latestBuildTime: indianTime,
 };
 
