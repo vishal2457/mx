569e4517fe738fd33d7aa5f99dbca1623f99def9
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2024-08-31 01:48:59 +0530

    chore: member app build added

--- apps/admin/src/app/shared/ui/form/mx-input.ts
+++ apps/admin/src/app/shared/ui/form/mx-input.ts
@@ -40,7 +40,7 @@ import { MxFormErrorComponent } from './form-error';
         </span>
       }
       <input
-        class="flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+        class="dark:[color-scheme:dark] flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
         autocomplete="off"
         [id]="_id"
         [ngClass]="_inputClass()"
