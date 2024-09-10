4de55bc988dcead249d9ab9ff58a120bd3dfd90b
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2024-09-10 21:48:15 +0530

    chore: member app build added

--- apps/api/src/express-app.ts
+++ apps/api/src/express-app.ts
@@ -11,22 +11,22 @@ import responseTime from 'response-time';
 import promClient from 'prom-client';
 
 // Use default collection metric and register it with promethus client
-const collectDefcollectDefaultMetrics = promClient.collectDefaultMetrics;
-collectDefcollectDefaultMetrics({
-  register: promClient.register,
-});
+// const collectDefcollectDefaultMetrics = promClient.collectDefaultMetrics;
+// collectDefcollectDefaultMetrics({
+//   register: promClient.register,
+// });
 
-const reqResTime = new promClient.Histogram({
-  name: 'http_express_req_res_time',
-  help: 'This tells how much time is taken by request and response',
-  labelNames: ['method', 'route', 'status_code'],
-  buckets: [1, 50, 100, 200, 400, 500, 800, 1000, 2000],
-});
+// const reqResTime = new promClient.Histogram({
+//   name: 'http_express_req_res_time',
+//   help: 'This tells how much time is taken by request and response',
+//   labelNames: ['method', 'route', 'status_code'],
+//   buckets: [1, 50, 100, 200, 400, 500, 800, 1000, 2000],
+// });
 
-const totalReqCounter = new promClient.Counter({
-  name: 'total_req',
-  help: 'Total requests',
-});
+// const totalReqCounter = new promClient.Counter({
+//   name: 'total_req',
+//   help: 'Total requests',
+// });
 
 const app = express();
 
@@ -46,19 +46,19 @@ app
     }),
   )
   .use('/static', express.static(path.join(process.cwd() + '/mx-images/')))
-  .use(logHttpRequests)
-  .use(
-    responseTime((req, res, time) => {
-      totalReqCounter.inc();
-      reqResTime
-        .labels({
-          method: req.method,
-          route: req.url,
-          status_code: res.statusCode,
-        })
-        .observe(time);
-    }),
-  );
+  .use(logHttpRequests);
+// .use(
+//   responseTime((req, res, time) => {
+//     totalReqCounter.inc();
+//     reqResTime
+//       .labels({
+//         method: req.method,
+//         route: req.url,
+//         status_code: res.statusCode,
+//       })
+//       .observe(time);
+//   }),
+// );
 
 //init all the modules
 app.use('/api/v1', routerv1);
